caff6d1623a75ef5c031815ee4636d23eaef72bd
Flandia <me@flandia.dev>
2026-08-10 03:45:56 -0400

    chore: clean v1 files for v2

--- internal/pkg/arkprocessor/processor.go
+++
@@ -1,8 +0,0 @@
-package arkprocessor
-
-// Processor provides functionalities for processing art. The term "process"
-// here refers to merging color channel and alpha channel of arts together,
-// merging faces variation and bodies of character arts together, etc.
-type Processor struct {
-	Root string
-}
--- internal/pkg/arkscanner/scanner.go
+++
@@ -1,5 +0,0 @@
-package arkscanner
-
-type Scanner struct {
-	Root string
-}
